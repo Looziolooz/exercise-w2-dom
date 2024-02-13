ad5936f699018008bd988b6183745b6747a8cfa6
loren <lorenzo.dastoli@gmail.com>
2024-02-13 20:14:35 +0100

    target options - listener dark Theme

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 console.log("Start Here 😉");
+
+//target options
+const darkTheme = document.getElementById ('dark');
+const lightheme = document.getElementById ('light');
+const darkTheme = document.getElementById ('crazy');
+const box = document.querySelector ('.box');
+
+//listeners
+
+darkTheme.addEventListener('change', function()) {
+  if (darkTheme.click) {
+    changeTheme('dark')
+  }
+}
